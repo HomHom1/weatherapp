9a73b1c445dec4d928a079ecf2df0d4ad4139fb5
HomHom1 <homahomen2@gmail.com>
2022-09-14 22:24:29 -0500

    new geolocation

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -40,6 +40,31 @@ function Homepage() {
   );
 };
 
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=f2ec7956667dfed47136d88606db9b0a`)
+      .then((response) => setPlaceInfo({
+        name: response.data.name,
+        country: response.data.sys.country,
+        weather: {
+          current: response.data.main.temp,
+          max: response.data.main.temp_max,
+          min: response.data.main.temp_min,
+          feel: response.data.main.feels_like
+        },
+        condition: response.data.weather[0].main
+      }))
+    });
+
+  }
+}
+
+
+
+
+
   const getSetHandle = (city) => {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f2ec7956667dfed47136d88606db9b0a`)
     .then((response) => setPlaceInfo({
@@ -72,8 +97,8 @@ function Homepage() {
               getSetHandle('New York')
             }}><h4>New York</h4></button>
             <button className='example-city' onClick={() => {
-              getSetHandle('Moscow')
-            }}><h4>Moscow</h4></button>
+              getLocation()
+            }}><h4>Locate me!</h4></button>
           </div>
         </div>
       </div>
@@ -104,20 +129,20 @@ function Homepage() {
           <h3> 
             <img src={maxtemp} alt='' width='25'/>
               
-              Max: {Math.round((placeInfo?.weather.max - 273.15) * 9/5 +32 )}˚ F
+              Max: {Math.round((placeInfo?.weather.max - 273.15) * (9/5) + 32 )}˚ F
           </h3>
 
           <h3>
             <img src={mintemp} alt='' width='25'/>
             
-            Min: {Math.round((placeInfo?.weather.min - 273.15) *9/5 +32 )}˚ F
+            Min: {Math.round((placeInfo?.weather.min - 273.15) *(9/5) +32 )}˚ F
 
           </h3>
           
           <h3>
             <img src={feelslike} alt='' width='25'/>
             
-            Feel-like: {Math.round((placeInfo?.weather.feel - 273.15) * 9/5 +32)}˚ F
+            Feel-like: {Math.round((placeInfo?.weather.feel - 273.15) * (9/5) +32)}˚ F
             
           </h3>
         </div>
